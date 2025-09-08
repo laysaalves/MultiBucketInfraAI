b8287babc64f0796d0ec9ee1454d8580506abdd4
Laysa <laysa.developer@gmail.com>
2025-09-08 14:27:04 -0300

    add initial mcp client

---
+++ MultiBucketInfraAIClient/MultiBucketInfraAIClient/src/main/java/dev/layseiras/MultiBucketInfraAIClient/MultiBucketInfraAiClientApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.MultiBucketInfraAIClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiBucketInfraAiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiBucketInfraAiClientApplication.class, args);
+	}
+
+}
---
+++ MultiBucketInfraAIClient/MultiBucketInfraAIClient/src/test/java/dev/layseiras/MultiBucketInfraAIClient/MultiBucketInfraAiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.MultiBucketInfraAIClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBucketInfraAiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
